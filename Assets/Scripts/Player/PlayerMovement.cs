24c994ad77a28ba25d31117aa824b1aa078965ef
F4dL <fadl2009gh@gmail.com>
2023-03-29 16:35:45 +0300

    First implementation of player controller #1

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        rb.AddForce(Vector2.right * speed * horizontal, ForceMode2D.Impulse);
+    }
+}
